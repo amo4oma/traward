2b9d9e724170e62d510824252ca5b4ffb8f29e98
Ahmed Obad <63117335+amo4oma@users.noreply.github.com>
2022-05-18 17:59:47 +0800

    all

---
+++ gatsby-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  pathPrefix: '/traworld/',
+  siteMetadata: {
+    siteUrl: 'http://creativeunicorn.com/staging',
+    title: 'Traworld',
+  },
+  plugins: [
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+  ],
+  
+};
